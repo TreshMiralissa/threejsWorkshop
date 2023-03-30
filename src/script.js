ab7a03c6d4c23f183402a398cc28377b7a12a28e
TreshMiralissa <muratet.jade@yahoo.com>
2023-03-30 10:16:23 +0200

    package + scene + camera

--- src/script.js
+++ src/script.js
@@ -1,1 +1,28 @@
-console.log("wat up");
+import * as THREE from 'three';
+
+// scene
+const scene = new THREE.Scene();
+
+const geometry = new THREE.BoxGeometry(1, 1, 1);
+const material = new THREE.MeshBasicMaterial({color:'olivedrab'});
+const mesh = new THREE.Mesh(geometry, material);
+// ajout du mesh sur la scene
+scene.add(mesh);
+
+// camera
+const sizes = {
+    with: 800,
+    height:600,
+}
+const camera = new THREE.PerspectiveCamera(75, sizes.with/ sizes.height);
+camera.position.set(0,0,3);
+// ajout de la camera sur la scene
+scene.add(camera);
+
+// rendu
+const canvas = document.querySelector('.webgl');
+const renderer = new THREE.WebGL1Renderer({
+    canvas: canvas
+});
+renderer.setSize(sizes.with, sizes.height)
+renderer.render(scene, camera);
